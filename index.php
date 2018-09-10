f624066a4dafca856601f4b3048116546e9d2f6f
Paradoxis <luke@paradoxis.nl>
2018-09-10 20:52:25 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta name="google-site-verification" content="vGZ4LMCA8Mu7egf-kqi9-Ti0qd0r-0tvj3bAV1_XqTw" />
+
+                <title>Benefietveiling oneMen</title>
+		<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+ 
+		<link rel="stylesheet" type="text/CSS" href="plugins/bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/CSS" href="plugins/flatui/css/flat-ui.css" />
+		<link rel="stylesheet" type="text/CSS" href="css/index.css" />
+
+		<script type="text/javascript" src="plugins/jquery/jquery.js"></script>
+		<script type="text/javascript" src="plugins/jquery/jquery.easing.js"></script>
+		<script type="text/javascript" src="plugins/jquery/jquery.transit.js"></script>
+		<script type="text/javascript" src="plugins/bootstrap/js/bootstrap.js"></script>
+		<script type="text/javascript" src="plugins/flatui/js/flatui.js"></script>
+		<script type="text/javascript" src="plugins/flatui/js/flatui-checkbox.js"></script>
+		<script type="text/javascript" src="plugins/flatui/js/flatui-radio.js"></script>
+		<link rel="stylesheet" type="text/css" href="http://fast.fonts.net/cssapi/b747e605-8589-43f3-a3c4-09648cb7bbb9.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- Arina main JS script -->
+
+		<script type="text/javascript" src="js/index.js"></script>
+	</head>
+	<body >
+		
+		<!-- main -->
+		<header class="v-center-wrapper">
+			<div class="container v-center">
+				<div class="col-md-12">
+					<div class="row text-center">
+						<img src="images/logo.png" alt="onemen logo" />
+						<h2>Benefietveiling</h2>
+					</div>
+				</div>
+			</div>
+		</header>
+		
+		<!-- wrapper -->
+		<main>
+
+			<!-- missie en visie -->
+			<section id="missie-en-visie">
+				<div class="container">
+					<!-- title -->
+					<div class="row">
+						<div class="col-md-12">
+							<h2>MISSIE EN VISIE</h2>
+						</div>
+					</div>
+					
+					<!-- yt video + text left -->
+					<div class="row">
+						<!-- youtube video -->
+						<div class="col-lg-5 col-md-6">
+							<div id="yt-wrapper">
+								<iframe width="560" height="315" src="//www.youtube-nocookie.com/embed/7iOogndPQQc?controls=0&showinfo=0&rel=0&iv_load_policy=3" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+
+						<!-- text -->
+						<div class="col-lg-7 col-md-6" id="missie-text-main">
+							<p>
+								OneMen helpt pioniers in Afrika, Azië en Latijns-Amerika.
+								Pioniers zijn mensen die zich inzetten voor de local community in hun eigen land.
+								Met lef en doorzettingsvermogen, soms zelfs met gevaar voor eigen leven.
+								<br />
+								Ze zijn actief op veel terreinen, zoals 
+								<a href="http://www.onemen.org/werkvelden/vrouwenrechten">vrouwenrechten</a>, 
+								<a href="http://www.onemen.org/werkvelden/mensenrechten">mensenrechten</a>,
+								<a href="http://www.onemen.org/werkvelden/kinderrechten">rechten van kinderen</a>, 
+								<a href="http://www.onemen.org/werkvelden/gezondheidszorg-in-afrika">gezondheidszorg</a> en 
+								<a href="http://www.onemen.org/werkvelden/onderwijs-in-ontwikkelingslanden">onderwijs</a>.
+								OneMen pioniers maken een verschil, in kleinschalige projecten met grote impact.
+							</p>
+						</div>
+
+						<div class="clearfix visible-lg-block"></div>
+						<div class="col-lg-offset-5 col-lg-7 col-md-12" id="yt-fix">
+							<p>
+								Het is onze missie om deze <a href="http://www.onemen.org/steun-een-pionier">pioniers te helpen met concrete middelen</a> op het gebied van transport en communicatie: een auto, fiets, ezel, boot, computer of bus.
+								Deze praktische middelen zijn van onschatbare waarde voor de pioniers: hiermee kunnen zij veel meer mensen bereiken en mensen in een groter gebied helpen.<br />
+							</p>
+						</div>
+					</div>
+					
+					
+					<!-- text bottom -->
+					<div class="row">
+						<div class="col-md-12" id="mv-bottomtxt">
+							<p>
+								OneMen gelooft in de kracht van het individu. In pioniers: zij streven er naar om van de wereld een betere plek te maken. 
+								En in jou: de supporter die deze pioniers helpt vanuit Nederland, <a href="http://www.onemen.org/wat-kan-ik-doen">op jouw eigen manier</a>.  
+								OneMen can make a difference!
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+			
+			<!-- Pionier Michele -->
+			<section id="pionier-michelle">
+				<div class="container">
+					<!-- title -->
+					<div class="row">
+						<div class="col-md-12">
+							<h2>Pionier Michele</h2>					
+						</div>
+					</div>
+					
+					<!-- text -->
+					<div class="row">
+						<div class="col-md-8">						
+							<p>
+								Pionier Michele helpt straatkinderen in Brazilië
+							</p>
+
+							<p>
+								OneMen pionier Michele zet zich in voor kwetsbare kinderen in gevaarlijke buurten.
+								In Brazilië is geweld aan de orde van de dag.
+								Drugsbazen vermoorden elkaar en tiranniseren de buurt.
+								<br />
+								Michele voorkomt met zijn onderwijsproject dat kleine kinderen in handen van criminelen vallen en geronseld worden als drugsrunner.
+								Hij heeft met spoed een auto nodig, zodat hij de gevaarlijke gebieden kan betreden en in staat is de onderwijsprojecten te bezoeken.
+								<br />
+								Michele vertelt: "Geweld is hier een groot issue. Veel jongeren, vaak kinderen nog, hebben zware wapens en vechten tegen elkaar. 
+								Laatst waren er 14 moorden in een maand tijd! Wij leren de kinderen op school dat er ook andere manieren zijn om met conflicten om te gaan.
+								Bij ons mogen ze even kind zijn." 
+							</p>
+						</div>
+
+						<div class="col-md-4">
+							<img src="images/pionier.png" alt="Pionier Michele" />
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+			<!-- tickets , locatie -->
+			<section id="tickets-locatie">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h2>Tickets en Locatie</h2>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-7">
+							<table class="table table-hover table-bordered">
+								<tr>
+									<th>Entree</th>
+									<td>&euro; 5,00 per persoon</td>
+								</tr>
+
+								<tr>
+									<th>Betaalwijze</th>
+									<td>Pin &amp; Cash</td>
+								</tr>
+
+								<tr>
+									<th>Dresscode</th>
+									<td>Casual chique</td>
+								</tr>
+
+								<tr>
+									<th>Datum en Tijd</th>
+									<td>10 oktober 2014, 18:00 - 22:00</td>
+								</tr>
+
+								<tr>
+									<th>Adres</th>
+									<td>
+										Het Volkshuis
+										Apothekersdijk 33a
+										2312RA Leiden
+									</td>
+								</tr>
+							</table>
+						</div>
+
+
+						<div class="col-md-5">
+							<p>
+								Steun pionier Michele en de straatkinderen in Brazilië en bied mee tijdens de Benefietveiling! <br />
+								Tijdens deze fantastische veiling worden verschillende producten geveild, er zal muziek gespeeld worden en natuurlijk kan dit alles onder het genot van een hapje en een drankje. <br />
+								De complete opbrengst van de benefietveiling komt ten goede aan oneMen en haar pionier Michele.
+							</p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">
+							<p>
+								Verzeker uzelf van een plek op de gastenlijst, u kunt dit doen door een email te sturen of op Facebook aan te geven door op de event pagina dat u langs komt!
+							</p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-6">
+							<a class="btn 
+btn-block btn-primary" href="(redacted)">
+							    <span class="fui-mail"></span> 
+							    Laat het ons weten via E-Mail
+							</a>
+						</div>
+
+						<div class="col-md-6">
+							<a class="btn btn-block btn-facebook" href="https://www.facebook.com/events/341459649366462/">
+							    <span class="fui-facebook"></span> 
+							    Laat het ons weten via Facebook
+							</a>
+						</div>
+					</div>
+
+					<!-- google maps -->
+					<div class="row">
+						<div class="col-md-12">
+							<div id="map_canvas"></div>
+						</div>
+					</div>
+					
+				</div>
+			</section>
+
+
+			<!-- footer -->
+			<footer class="palette-midnight-blue">
+				<div class="container">
+					<div class="col-md-8 col-xs-12">
+						<h3>Contact</h3>
+						<table>
+							<tr>
+								<td>Support email:</td>
+								<td><a href="mailto:(redacted)">(redacted)</a></td>
+							</tr>
+
+							<tr>
+								<td>Support telefonisch:</td>
+								<td>+31 (0)6 (redacted)</td>
+							</tr>
+
+							<tr>
+								<td>Aanspreek persoon:</td>
+								<td>Nina Paris</td>
+							</tr>
+						</table>
+					</div>
+
+					<div class="col-md-4 col-xs-12 palette-wet-asphalt">
+						<h3>Website</h3>
+						<table>
+							<tr>
+								<td>Developer website:</td>
+								<td><a href="http://paradoxis.nl">www.paradoxis.nl</a></td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</footer>
+		</main>
+
+		<script src="https://maps.googleapis.com/maps/api/js"></script>
+		<script>
+		function initialize() {
+			var mapCanvas = document.getElementById('map_canvas');
+
+			var mapOptions = {
+				center 		: new google.maps.LatLng(52.1606024, 4.4883606),
+				zoom 		: 15,
+				mapTypeId 	: google.maps.MapTypeId.ROADMAP
+			}
+
+			var map 	= new google.maps.Map(mapCanvas, mapOptions);
+
+			var marker  = new google.maps.Marker({
+				position	: new google.maps.LatLng(52.1606024, 4.4883606),
+				map 		: map,
+				title 		: 'Het Volkshuis Leiden'
+			});
+		}
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+		</script>
+	</body>
+</html>
